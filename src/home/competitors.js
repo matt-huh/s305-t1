85ceb58d9a7f75013c4a84b24876b60390117bba
matt.huh <matt.huh@outlook.com>
2021-11-14 23:27:54 -0500

    Add competitors data

---
+++ src/home/competitors.js
@@ -0,0 +1,30 @@
+export const competitors = [
+  {
+    name: "Starlink",
+    download: 200,
+    upload: 42,
+    latency: 20,
+    satellites: 1646,
+  },
+  {
+    name: "Kuiper",
+    download: 400,
+    upload: "Not Revealed",
+    latency: "Not Revealed",
+    satellites: "Planned to launch in late 2022",
+  },
+  {
+    name: "Oneweb",
+    download: 165,
+    upload: 30,
+    latency: 45,
+    satellites: 358,
+  },
+  {
+    name: "Telestat",
+    download: 50,
+    upload: 10,
+    latency: 30,
+    satellites: 298,
+  },
+];
